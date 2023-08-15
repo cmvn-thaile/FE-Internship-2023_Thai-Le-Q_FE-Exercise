af29e61fd701167590fde1b2a5eb8bb3d55a6d62
cmvn-thaile <thai.le@classmethod.vn>
2023-08-15 10:04:03 +0700

    fix bug for exercise 2

--- js-basic/2.js
+++ js-basic/2.js
@@ -11,12 +11,12 @@
  */
 function occurrenceOfSubstring(str, subStr) {
   let count = 0;
-  let index = str.indexOf(subStr);
+  let index = str.toLocaleLowerCase().indexOf(subStr);
   while (index !== -1) {
     count++;
     index = str.indexOf(subStr, index + 1);
   }
   return count;
 }
-// const result2 = occurrence("str str str sfs", "str");
-// console.log(result2);
+const result2 = occurrenceOfSubstring("the quick brown fox jumps over the lazy dog", "the");
+console.log(result2);
